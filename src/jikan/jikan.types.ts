0cc806a44096f45d1aa30101f5107dca0cd9c8ea
Jeremy-Park <32861944+Jeremy-Park@users.noreply.github.com>
2025-05-31 00:33:19 +0900

    Create jikan.types.ts
    - Add types for Jikan API response

---
+++ src/jikan/jikan.types.ts
@@ -0,0 +1,99 @@
+// Based on https://docs.api.jikan.moe/
+
+export interface JikanApiResponse<T> {
+  data: T;
+}
+
+export interface Anime {
+  mal_id: number;
+  url: string;
+  images: {
+    jpg: {
+      image_url: string;
+      small_image_url: string;
+      large_image_url: string;
+    };
+    webp: {
+      image_url: string;
+      small_image_url: string;
+      large_image_url: string;
+    };
+  };
+  trailer: {
+    youtube_id: string | null;
+    url: string | null;
+    embed_url: string | null;
+    images: {
+      image_url: string | null;
+      small_image_url: string | null;
+      medium_image_url: string | null;
+      large_image_url: string | null;
+      maximum_image_url: string | null;
+    };
+  };
+  title: string;
+  title_english: string | null;
+  title_japanese: string | null;
+  title_synonyms: string[];
+  type: string | null;
+  source: string | null;
+  episodes: number | null;
+  status: string | null;
+  airing: boolean;
+  aired: {
+    from: string | null;
+    to: string | null;
+    prop: {
+      from: { day: number | null; month: number | null; year: number | null };
+      to: { day: number | null; month: number | null; year: number | null };
+    };
+    string: string | null;
+  };
+  duration: string | null;
+  rating: string | null;
+  score: number | null;
+  scored_by: number | null;
+  rank: number | null;
+  popularity: number | null;
+  members: number | null;
+  favorites: number | null;
+  synopsis: string | null;
+  background: string | null;
+  season: string | null;
+  year: number | null;
+  broadcast: {
+    day: string | null;
+    time: string | null;
+    timezone: string | null;
+    string: string | null;
+  };
+  producers: Array<{ mal_id: number; type: string; name: string; url: string }>;
+  licensors: Array<{ mal_id: number; type: string; name: string; url: string }>;
+  studios: Array<{ mal_id: number; type: string; name: string; url: string }>;
+  genres: Array<{ mal_id: number; type: string; name: string; url: string }>;
+  explicit_genres: Array<{
+    mal_id: number;
+    type: string;
+    name: string;
+    url: string;
+  }>;
+  themes: Array<{ mal_id: number; type: string; name: string; url: string }>;
+  demographics: Array<{
+    mal_id: number;
+    type: string;
+    name: string;
+    url: string;
+  }>;
+  relations: Array<any>; // Define more specific type if needed
+  external: Array<{ name: string; url: string }>;
+  streaming: Array<{ name: string; url: string }>;
+}
+
+export interface AnimePicture {
+  jpg: {
+    image_url: string;
+  };
+  webp: {
+    image_url: string;
+  };
+}
