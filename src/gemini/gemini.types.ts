080a4afc7ad95e10ee0f67b44a13665b09236c0a
Jeremy-Park <32861944+Jeremy-Park@users.noreply.github.com>
2025-05-31 00:35:01 +0900

    Update gemini.types.ts
    - Add jikan tool names

--- src/gemini/gemini.types.ts
+++ src/gemini/gemini.types.ts
@@ -1,6 +1,10 @@
 export enum GeminiToolName {
   GET_CURRENT_WEATHER = 'get_current_weather',
   GET_USER_LOCATION = 'get_user_location',
+  GET_ANIME_BY_ID = 'get_anime_by_id',
+  GET_ANIME_SEARCH = 'get_anime_search',
+  GET_ANIME_PICTURES = 'get_anime_pictures',
+  GET_TOP_ANIME = 'get_top_anime',
 }
 
 export interface GeminiFunctionCall {
