4a115bb84d6340b1de9c5ad5fd8c510bdb3d6726
Jeremy-Park <32861944+Jeremy-Park@users.noreply.github.com>
2025-05-30 23:17:33 +0900

    Remove weather controller

--- src/weather/weather.module.ts
+++ src/weather/weather.module.ts
@@ -2,14 +2,13 @@ import { HttpModule } from '@nestjs/axios';
 import { Module } from '@nestjs/common';
 import { GeminiModule } from '../gemini/gemini.module';
 import { GeocodingModule } from '../geocoding/geocoding.module';
-import { WeatherController } from './weather.controller';
 import { WeatherService } from './weather.service';
 
 // ----------------------------------------------------------------------
 
 @Module({
   imports: [HttpModule, GeminiModule, GeocodingModule],
-  controllers: [WeatherController],
+  controllers: [],
   providers: [WeatherService],
   exports: [WeatherService],
 })
