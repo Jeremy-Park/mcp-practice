ff2aa1b95da35fecf5c00a1788bd03ba0e91c69e
Jeremy-Park <32861944+Jeremy-Park@users.noreply.github.com>
2025-05-30 23:26:24 +0900

    Update gemini.types.ts
    - Add enum for tool names

--- src/gemini/gemini.types.ts
+++ src/gemini/gemini.types.ts
@@ -1,3 +1,8 @@
+export enum GeminiToolName {
+  GET_CURRENT_WEATHER = 'get_current_weather',
+  GET_USER_LOCATION = 'get_user_location',
+}
+
 export interface GeminiFunctionCall {
   name: string;
   args: Record<string, any>;
