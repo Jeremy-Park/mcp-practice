9ee3744b6d36be2493066cd6c0c1a8e58a1918c1
Jeremy-Park <32861944+Jeremy-Park@users.noreply.github.com>
2025-05-30 22:19:33 +0900

    Extract session management as its own feature
    - Add logs to each service
    - Add potential new methods

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,12 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { WeatherModule } from './weather/weather.module';
 import { GeminiModule } from './gemini/gemini.module';
 import { GeocodingModule } from './geocoding/geocoding.module';
+import { SessionModule } from './session/session.module';
+import { WeatherModule } from './weather/weather.module';
+
+// ----------------------------------------------------------------------
 
 @Module({
   imports: [
@@ -12,9 +15,10 @@ import { GeocodingModule } from './geocoding/geocoding.module';
       isGlobal: true,
       envFilePath: '.env',
     }),
-    WeatherModule,
     GeminiModule,
     GeocodingModule,
+    SessionModule,
+    WeatherModule,
   ],
   controllers: [AppController],
   providers: [AppService],
