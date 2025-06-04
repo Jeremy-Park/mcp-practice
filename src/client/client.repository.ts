774b5c5ad3ad8d5eb4ff4b8fc3eb8e3b9a27e0ab
Jeremy-Park <32861944+Jeremy-Park@users.noreply.github.com>
2025-06-04 20:30:24 +0900

    Update client.repository.ts
    Comment out

--- src/client/client.repository.ts
+++ src/client/client.repository.ts
@@ -1,107 +1,106 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
-import { Prisma, Client, TeamClient } from '../../generated/prisma';
 
-type ClientWithTeamsPayload = Prisma.ClientGetPayload<{
-  include: { teams: { include: { team: true } } }
-}>;
+// type ClientWithTeamsPayload = Prisma.ClientGetPayload<{
+//   include: { teams: { include: { team: true } } }
+// }>;
 
-type TeamClientWithClientPayload = Prisma.TeamClientGetPayload<{
-  include: { client: true }
-}>;
+// type TeamClientWithClientPayload = Prisma.TeamClientGetPayload<{
+//   include: { client: true }
+// }>;
 
 @Injectable()
 export class ClientRepository {
   constructor(private readonly prisma: PrismaService) {}
 
-  async create(data: Prisma.ClientCreateInput): Promise<Client> {
-    return this.prisma.client.create({ data });
-  }
+  // async create(data: Prisma.ClientCreateInput): Promise<Client> {
+  //   return this.prisma.client.create({ data });
+  // }
 
-  async findById(id: number): Promise<ClientWithTeamsPayload | null> {
-    return this.prisma.client.findUnique({
-      where: { id, deleted: false },
-      include: { teams: { where: { team: { deleted: false }}, include: { team: true } } },
-    });
-  }
+  // async findById(id: number): Promise<ClientWithTeamsPayload | null> {
+  //   return this.prisma.client.findUnique({
+  //     where: { id, deleted: false },
+  //     include: { teams: { where: { team: { deleted: false }}, include: { team: true } } },
+  //   });
+  // }
 
-  async findAll(teamId?: number): Promise<ClientWithTeamsPayload[]> {
-    const whereClause: Prisma.ClientWhereInput = { deleted: false };
-    if (teamId) {
-      whereClause.teams = {
-        some: {
-          teamId: teamId,
-          team: { deleted: false }
-        }
-      }
-    }
-    return this.prisma.client.findMany({
-      where: whereClause,
-      include: { teams: { where: { team: {deleted: false}}, include: { team: true } } }
-    });
-  }
+  // async findAll(teamId?: number): Promise<ClientWithTeamsPayload[]> {
+  //   const whereClause: Prisma.ClientWhereInput = { deleted: false };
+  //   if (teamId) {
+  //     whereClause.teams = {
+  //       some: {
+  //         teamId: teamId,
+  //         team: { deleted: false }
+  //       }
+  //     }
+  //   }
+  //   return this.prisma.client.findMany({
+  //     where: whereClause,
+  //     include: { teams: { where: { team: {deleted: false}}, include: { team: true } } }
+  //   });
+  // }
 
-  async update(id: number, data: Prisma.ClientUpdateInput): Promise<Client> {
-    return this.prisma.client.update({
-      where: { id, deleted: false },
-      data,
-    });
-  }
+  // async update(id: number, data: Prisma.ClientUpdateInput): Promise<Client> {
+  //   return this.prisma.client.update({
+  //     where: { id, deleted: false },
+  //     data,
+  //   });
+  // }
 
-  async softDelete(id: number): Promise<Client> {
-    return this.prisma.client.update({
-      where: { id },
-      data: { deleted: true },
-    });
-  }
+  // async softDelete(id: number): Promise<Client> {
+  //   return this.prisma.client.update({
+  //     where: { id },
+  //     data: { deleted: true },
+  //   });
+  // }
 
-  // Methods for TeamClient relationships
-  async assignClientToTeam(clientId: number, teamId: number): Promise<TeamClient> {
-    return this.prisma.teamClient.create({
-      data: {
-        clientId,
-        teamId,
-      },
-    });
-  }
+  // // Methods for TeamClient relationships
+  // async assignClientToTeam(clientId: number, teamId: number): Promise<TeamClient> {
+  //   return this.prisma.teamClient.create({
+  //     data: {
+  //       clientId,
+  //       teamId,
+  //     },
+  //   });
+  // }
 
-  async findTeamClientAssignment(clientId: number, teamId: number): Promise<TeamClient | null> {
-    return this.prisma.teamClient.findUnique({
-      where: {
-        teamId_clientId: { teamId, clientId },
-        team: { deleted: false },
-        client: { deleted: false }
-      }
-    });
-  }
+  // async findTeamClientAssignment(clientId: number, teamId: number): Promise<TeamClient | null> {
+  //   return this.prisma.teamClient.findUnique({
+  //     where: {
+  //       teamId_clientId: { teamId, clientId },
+  //       team: { deleted: false },
+  //       client: { deleted: false }
+  //     }
+  //   });
+  // }
 
-  async removeClientFromTeam(clientId: number, teamId: number): Promise<TeamClient> {
-    return this.prisma.teamClient.delete({
-      where: {
-        teamId_clientId: { teamId, clientId },
-      },
-    });
-  }
+  // async removeClientFromTeam(clientId: number, teamId: number): Promise<TeamClient> {
+  //   return this.prisma.teamClient.delete({
+  //     where: {
+  //       teamId_clientId: { teamId, clientId },
+  //     },
+  //   });
+  // }
 
-  async findClientsByTeamId(teamId: number): Promise<TeamClientWithClientPayload[]> {
-    return this.prisma.teamClient.findMany({
-      where: {
-        teamId,
-        team: { deleted: false },
-        client: { deleted: false }
-      },
-      include: { client: true },
-    });
-  }
+  // async findClientsByTeamId(teamId: number): Promise<TeamClientWithClientPayload[]> {
+  //   return this.prisma.teamClient.findMany({
+  //     where: {
+  //       teamId,
+  //       team: { deleted: false },
+  //       client: { deleted: false }
+  //     },
+  //     include: { client: true },
+  //   });
+  // }
 
-  async findTeamsByClientId(clientId: number): Promise<Prisma.TeamClientGetPayload<{include: {team: true}}>[]> {
-    return this.prisma.teamClient.findMany({
-      where: {
-        clientId,
-        client: { deleted: false },
-        team: { deleted: false }
-      },
-      include: { team: true },
-    });
-  }
-} 
+  // async findTeamsByClientId(clientId: number): Promise<Prisma.TeamClientGetPayload<{include: {team: true}}>[]> {
+  //   return this.prisma.teamClient.findMany({
+  //     where: {
+  //       clientId,
+  //       client: { deleted: false },
+  //       team: { deleted: false }
+  //     },
+  //     include: { team: true },
+  //   });
+  // }
+}
