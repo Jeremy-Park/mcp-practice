153c4491c16d6cb9d413b3978ba038b7474b1902
Jeremy-Park <32861944+Jeremy-Park@users.noreply.github.com>
2025-06-04 20:14:50 +0900

    Update prisma.module.ts
    Clean up

--- src/prisma/prisma.module.ts
+++ src/prisma/prisma.module.ts
@@ -1,9 +1,11 @@
 import { Global, Module } from '@nestjs/common';
 import { PrismaService } from './prisma.service';
 
-@Global() // Make PrismaService available globally
+// ----------------------------------------------------------------------
+
+@Global()
 @Module({
   providers: [PrismaService],
   exports: [PrismaService],
 })
-export class PrismaModule {} 
+export class PrismaModule {}
