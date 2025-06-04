52b1b835cc70a014fa589ed625c0b09a323f2609
Jeremy-Park <32861944+Jeremy-Park@users.noreply.github.com>
2025-06-04 21:21:16 +0900

    Update gemini.types.ts
    Sort enum

--- src/gemini/gemini.types.ts
+++ src/gemini/gemini.types.ts
@@ -1,13 +1,13 @@
 export enum GeminiToolName {
-  GET_CURRENT_WEATHER = 'get_current_weather',
-  GET_USER_LOCATION = 'get_user_location',
   GET_ANIME_BY_ID = 'get_anime_by_id',
-  GET_ANIME_SEARCH = 'get_anime_search',
   GET_ANIME_PICTURES = 'get_anime_pictures',
-  GET_TOP_ANIME = 'get_top_anime',
-  GET_GOOGLE_MAP = 'get_google_map',
+  GET_ANIME_SEARCH = 'get_anime_search',
+  GET_CURRENT_WEATHER = 'get_current_weather',
   GET_GOOGLE_DISTANCE = 'get_google_distance',
+  GET_GOOGLE_MAP = 'get_google_map',
   GET_MY_REALTOR_PROFILE = 'get_my_realtor_profile',
+  GET_TOP_ANIME = 'get_top_anime',
+  GET_USER_LOCATION = 'get_user_location',
 }
 
 export interface GeminiFunctionCall {
