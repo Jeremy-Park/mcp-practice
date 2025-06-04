c388db24590fc474fccecd39b7bc79aa8ee3b7f1
Jeremy-Park <32861944+Jeremy-Park@users.noreply.github.com>
2025-06-04 20:20:44 +0900

    Update realtor.repository.ts
    Remove comments

--- src/realtor/realtor.repository.ts
+++ src/realtor/realtor.repository.ts
@@ -8,26 +8,22 @@ import { PrismaService } from '../prisma/prisma.service';
 export class RealtorRepository {
   constructor(private readonly prisma: PrismaService) {}
 
-  // Create a new realtor
   async create(data: Prisma.RealtorCreateInput): Promise<Realtor> {
     return this.prisma.realtor.create({ data });
   }
 
-  // Get an active realtor by email
   async findByEmail(email: string): Promise<Realtor | null> {
     return this.prisma.realtor.findUnique({
       where: { deleted: false, email },
     });
   }
 
-  // Get an active realtor by ID
   async findById(id: number): Promise<Realtor | null> {
     return this.prisma.realtor.findUnique({
       where: { deleted: false, id },
     });
   }
 
-  // Update a realtor
   async update(id: number, data: Prisma.RealtorUpdateInput): Promise<Realtor> {
     return this.prisma.realtor.update({
       data,
